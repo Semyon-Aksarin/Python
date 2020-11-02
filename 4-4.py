0b6622f5ae2ce61dfe7ab4f7880f76e0009e402c
Semyon-Aksarin <72580198+Semyon-Aksarin@users.noreply.github.com>
2020-11-03 02:12:20 +0500

    Delete 4-4.py

--- 4-4.py
+++
@@ -1,7 +0,0 @@
-from random import randint
-
-my_list = [randint(0, 50) for i in range(50)]
-print(f'Список случайных 50 чисел от 0 до 50 - {my_list}')
-
-new_list = [i for i in my_list if my_list.count(i) == 1]
-print(f'Список чисел присутствующих в списке в 1 экземпляре - {new_list}')
